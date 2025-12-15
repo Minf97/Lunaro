bdfa37e1874a72a7b6ecabaea9f6a70c6a48fcc8
Minf <287728237@qq.com>
2025-12-15 21:27:24 +0800

    feat: sidebar init

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
-	import HelloWorld from '$lib/components/HelloWorld.svelte';
+	// import HelloWorld from '$lib/components/HelloWorld.svelte';
+	import AppSideBar from '$lib/components/AppSideBar.svelte';
+	import * as Sidebar from '$lib/components/ui/sidebar';
+
+	let { children } = $props();
 </script>
 
 <main
-	class="flex min-h-screen items-center justify-center bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 p-4 select-none"
+	class="flex min-h-screen select-none items-center justify-center bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 p-4"
 >
-	<HelloWorld />
+	<Sidebar.Provider>
+		<AppSideBar />
+		<main>
+			<Sidebar.Trigger />
+			{@render children?.()}
+		</main>
+	</Sidebar.Provider>
+	<!-- <HelloWorld /> -->
 </main>
